d30f1fa3b9afe18d11e5aea6e9d972ea2df3820b
Suguru Odai <ourqwers@gmail.com>
2012-08-10 20:18:32 +0900

    １回スペアだった場合のテスト追加

--- src/Boaling.js
+++ src/Boaling.js
@@ -1,9 +1,9 @@
-
 var Boaling =function() {
 };
 
 Boaling.prototype.throw = function(score) {
 	var total = 0;
+
 	for (var i = 0; i < score.length; i++) {
 		for (var j = 0; j < score[i].length; j++) {
 			total += score[i][j];
