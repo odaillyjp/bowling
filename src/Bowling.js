f30e4681f3582c56ebd48fc9a5e75f19fcd4e8d0
Suguru Odai <ourqwers@gmail.com>
2013-06-06 11:18:27 +0900

    resultメソッドのアルゴリズムを修正

--- src/Bowling.js
+++ src/Bowling.js
@@ -1,78 +1,29 @@
-function Bowling() {
-  this.totalScore = 0;
-  this.strike = [];
-  this.spare = [];
-};
+// (function() {
+ function Bowling() {}
+ Bowling.prototype = {
+   result: function(frames) {
+     var total = 0;
+     var myself = this;
+     frames.push([0])
+     frames.forEach(function(frame, idx) {
+       frame.forEach(function(score) { total += score; });
+       if (idx >= 9) { return; }
+       if (myself._isSpare(frame)) { total += frames[idx+1][0]; }
+       if (myself._isStrike(frame)) {
+         total += frames[idx+1][0]
+         if (frames[idx+1].length >= 2) { total += frames[idx+1][1]; }
+         if (myself._isStrike(frames[idx+1])) { total += frames[idx+2][0]; }
+       }
+     });
+     return total;
+   },
 
-Bowling.prototype = {
-  result : function(score) {
-    var frameTotal = 0;
-    var bonusScore = 0;
+   _isSpare: function(frame) {
+     return (frame[0] != 10) && (frame[0] + frame[1] == 10);
+   },
 
-    for (var i = 0; i < score.length; i++) {
-      frameTotal = this._frameResult(score[i]);
-      this.strike[i] = this._isStrike(score[i]);
-      this.spare[i] = this._isSpare(score[i]);
-      if (this.strike[9]) {
-        this.strike[10] = this._isStrike(score[9].slice(1));
-        this.spare[10] = this._isStrike(score[9].slice(1));
-      } else if (this.spare[9]) {
-        this.strike[10] = this._isStrike(score[9].slice(2));
-        this.spare[10] = false;
-      }
-
-      bonusScore = this._getBonusScore(i,score,frameTotal);
-      this.totalScore += (frameTotal + bonusScore);
-    }
-    return this.totalScore;
-  },
-
-  // Private method
-  _frameResult : function(score) {
-    var frameTotal = 0;
-    for (var i = 0; i < score.length; i++) {
-      frameTotal += score[i];
-    }
-    return frameTotal;
-  },
-
-  _getBonusScore : function(frame,score,frameTotal) {
-    var bonusScore = 0;
-    if (this.strike[(frame - 2)] && this.strike[(frame - 1)]) {
-      if (this.strike[9] && this.strike[10]) {
-        bonusScore = 30;
-      } else if (this.strike[9]) {
-        bonusScore = (20 + score[frame][1]);
-      } else if (this.spare[9]) {
-        bonusScore = (10 + score[frame][0]);
-      } else {
-        bonusScore = (frameTotal + score[frame][0]);
-      }
-    } else if (this.strike[(frame - 1)]) {
-      if ((this.strike[9]) || (this.strike[10])) {
-        bonusScore = 20;
-      } else if (this.strike[9]) {
-        bonusScore = (10 + score[frame][1]);
-      } else if (this.spare[9]) {
-        bonusScore = 10;
-      } else {
-        bonusScore = frameTotal;
-      }
-    } else if (this.spare[(frame - 1)]) {
-      bonusScore = score[frame][0];
-    }
-    return bonusScore;
-  },
-
-  _isStrike : function(score) {
-    var strikeFlag = false;
-    if (score[0] == 10) { strikeFlag = true; }
-    return strikeFlag;
-  },
-
-  _isSpare : function(score) {
-    var spareFlag = false;
-    if ((score[0] != 10) && (score[0] + score[1]) == 10) { spareFlag = true; }
-    return spareFlag;
-  }
-};
+   _isStrike: function(frame) {
+     return (frame[0] == 10);
+   }
+ }
+// })();
